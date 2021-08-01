1e250c1f51373e7b376811a3b19434f23b2e1456
TL <TL@gmail.com>
2021-08-01 08:55:07 +0530

    added class testjava.

---
+++ JAVA/src/com/pro/test/testjava.java
@@ -0,0 +1,12 @@
+package com.pro.test;
+
+import com.pro.beans.Utility;
+
+public class testjava {
+	
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println(u);
+	}
+
+}
